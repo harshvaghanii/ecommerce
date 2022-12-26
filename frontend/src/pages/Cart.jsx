60ebab4bdef9f71a601e84cfc77c916ce39f95d6
Harsh Vaghani <harshvaghani98@gmail.com>
2022-12-26 13:21:38 +0530

    Adding the login and register pages

---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div``;
+const Cart = () => {
+    return <Container>cart</Container>;
+};
+
+export default Cart;
